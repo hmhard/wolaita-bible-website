6a6f2017c1176da7e6ecca6e9ce61cc56c6cd182
Miniyahil Kebede <miniye6453@gmail.com>
2024-08-04 15:18:17 +0300

    home changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client"
+import { bibles } from "@/data/bible_names";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <p className=" text-3xl shadow-amber-500 shadow-lg">Welcome to wBB</p>
+  const router = useRouter();
+    
+  function handleOpenBibleName(bible: string): void {
 
+    router.push(`bible-content/${bible}`); 
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col justify-between p-24">
+      <div className=" text-4xl text-red-400 rounded-lg underline pb-3">Old Testaments</div>
+      <div className="grid grid-cols-4 gap-4">
+       
+       {bibles.map(bible =>
+        <div key={bible} onClick={()=>handleOpenBibleName(bible)} className="py-2 px-3 bg-slate-50 rounded-lg hover:bg-red-200 hover:text-white">{bible}</div>
+        )}
+
+      </div>
+      
     </main>
   );
 }
