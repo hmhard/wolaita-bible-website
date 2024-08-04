eb900a83798b2f26f76f8d06f25a80cb4dd68bbe
Miniyahil Kebede <miniye6453@gmail.com>
2024-08-04 23:57:13 +0300

    header and footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <div className="grid grid-cols-2 gap-4">
-        <div>
+    <main className="flex min-h-screen flex-col justify-between pt-10 pb-20 px-6">
+      <div className="grid grid-cols-2   gap-2">
+        <div className=" sm:col-span-12 lg:col-span-1">
           <div className=" text-4xl text-red-400 rounded-lg underline pb-3">Old Testaments</div>
-          <div className="grid grid-cols-4 gap-4">
+          <div className="lg:grid lg:grid-cols-4 sm:grid-cols-2 gap-4 rounded-lg shadow-lg px-2 pb-6 pt-2">
 
             {bibles.old_testaments.map(bible =>
               <div key={bible} onClick={() => handleOpenBibleName(bible)} className="py-2 px-3 bg-slate-50 rounded-lg hover:bg-red-200 hover:text-white">{bible}</div>
@@ -23,12 +23,12 @@ export default function Home() {
 
           </div>
         </div>
-        <div>
+        <div className=" sm:col-span-12 lg:col-span-1">
           <div className=" text-4xl text-red-400 rounded-lg underline pb-3">New Testaments</div>
-          <div className="grid grid-cols-4 gap-4">
+          <div className="lg:grid lg:grid-cols-4 sm:grid-cols-2 gap-2 rounded-lg shadow-lg px-2 pb-6 pt-2">
 
             {bibles.new_testaments.map(bible =>
-              <div key={bible} onClick={() => handleOpenBibleName(bible)} className="py-2 px-3 bg-slate-50 rounded-lg hover:bg-red-200 hover:text-white">{bible}</div>
+              <div key={bible} onClick={() => handleOpenBibleName(bible)} className="py-2 px-2 bg-slate-50 rounded-lg hover:bg-red-200 hover:text-white">{bible}</div>
             )}
 
           </div>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return <>
+    <div className=" text-center py-2">
+        <span>Copyright @2024 This Website Developed is By <a target="_blank" className=" underline" href="https://www.miniyahilkebede.com">Miniyahil Kebede</a></span>
+    </div>
+    </>;
+}
