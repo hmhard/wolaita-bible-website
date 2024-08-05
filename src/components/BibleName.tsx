33d825c6c21681c0c4bd6b626140d802255f10fa
Miniyahil Kebede <53700166+hmhard@users.noreply.github.com>
2024-08-05 12:52:15 +0300

    Change div to button , should be button (#31)
    * Change div to button , should be button
    
    * div to button

--- src/components/BibleName.tsx
+++ src/components/BibleName.tsx
@@ -6,16 +6,16 @@ export default function BibleName({ selectedBible, handleOpenBibleName }: { sele
     return <div className="grid grid-cols-2 gap-1 px-4">
         <TestamentText text="Old Testament"/>
         {bibles.old_testaments.map(bible =>
-            <div key={bible} onClick={() => handleOpenBibleName(bible)} className={`py-1 px-3 ${selectedBible === bible ? 'bg-red-400' : 'bg-slate-100'} rounded-lg hover:bg-red-200 hover:text-white`}>
+            <button key={bible} onClick={() => handleOpenBibleName(bible)} className={`py-1 px-3 ${selectedBible === bible ? 'bg-red-400' : 'bg-slate-100'} rounded-lg hover:bg-red-200 hover:text-white`}>
                 {bible}
-            </div>
+            </button>
         )}
         <TestamentText text="New Testament"/>
         {bibles.new_testaments.map(bible =>
 
-            <div key={bible} onClick={() => handleOpenBibleName(bible)} className={`py-1 px-3 ${selectedBible === bible ? 'bg-red-400' : 'bg-slate-100'} rounded-lg hover:bg-red-200 hover:text-white`}>
+            <button key={bible} onClick={() => handleOpenBibleName(bible)} className={`py-1 px-3 ${selectedBible === bible ? 'bg-red-400' : 'bg-slate-100'} rounded-lg hover:bg-red-200 shadow-lg hover:text-white`}>
                 {bible}
-            </div>
+            </button>
         )}
     </div>
 }
