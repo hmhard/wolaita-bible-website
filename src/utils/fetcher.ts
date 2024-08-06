62f6b9bda210bed4756a4601c857710fb3b219d9
Miniyahil Kebede <53700166+hmhard@users.noreply.github.com>
2024-08-06 22:51:11 +0300

    url updated for test (#35)

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,5 @@
 export  const fetcher = async(url:string) => {
 
-   var response = await fetch(`http://localhost:4200${url}`);
+   var response = await fetch(`https://api.samueltoma.com${url}`);
    return response.json();
 }
