80e43f52695d9f16a3f4bef5b29dfa1243d98bf7
Miniyahil Kebede <53700166+hmhard@users.noreply.github.com>
2024-09-11 02:57:12 +0300

    added order by to the books

--- src/app/api/v1/bible/books/route.ts
+++ src/app/api/v1/bible/books/route.ts
@@ -3,7 +3,7 @@ import prisma from "../../../../../../prisma/prisma-client";
 
 
 export async function GET() {
-    const posts = await prisma.bibleName.findMany({include:{testament:true}});
+    const posts = await prisma.bibleName.findMany({include:{testament:true}, orderBy: {id: 'asc'}});
 
 
     return NextResponse.json(posts);
